e0148b6f6d1c3fbcf00881938158aaa1d2baf72e
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-01-15 15:13:37 +0000

    Read and save search history to localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
-// Global array of objects containing city names and their longitudinal and latitudinal coordinates
-  // [{"name":"London", "lat":1.5073219, "lon":-0.1276474},{...},{...}]
+// Weather API Key
+const apiKey = "d06353babb67e713687d6023c8e892ff";
+
+// Maximum searches to save to history
+const maxHistory = 6;
+
+// Get historical search of city/place names and their longitudinal and latitudinal coordinates from localStorage or empty array if no data found (falsy)
+  // eg [{"name":"London", "lat":1.5073219, "lon":-0.1276474}, {...}, {...}]
+let searchHistory = JSON.parse(localStorage.getItem('weatherDashboard_searchHistory')) || [];
 
 // Function to search for city 
   // Use: https://openweathermap.org/api/geocoding-api to return lon/lat
@@ -13,10 +20,30 @@
       // Render buttons
 
 // Function to render the buttons
-    // Read the localStorage key into the global array
-      // Save lon and lat in the button data attributes. Display the city name on the button text. 
+function renderHistory(){
+  // Read the localStorage key into the global array
+  searchHistory = JSON.parse(localStorage.getItem('weatherDashboard_searchHistory')) || [];
+
+  // Save lon and lat in the button data attributes. Display the city name on the button text. 
+  for (i = 0; i < searchHistory.length; i++){
+    console.log(searchHistory[i].name);
+  }
+
+}
 
 // Function to save a city to the global array and localstorage (input: city name, lon, lat)
+function saveSearch(city, lon, lat) {
+
+  // Create search object
+  const search = {city: city, lon: lon, lat: lat};
+
+  // Save object to beginning of array of search history 
+  searchHistory.unshift(search)
+
+  // Save search history to localStorage
+  localStorage.setItem('weatherDashboard_searchHistory', JSON.stringify(searchHistory));
+
+}
 
 // Function to get and render current weather (input: lon, lat)
 
@@ -29,3 +56,7 @@
     // Display current weather (via function)
     // Display 5 day forecast (via function) 
 
+
+renderHistory();
+
+saveSearch("London", 1.5073219, -0.1276474);
